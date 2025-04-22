52a285d5deaff1eba8529c03f49045c1047357b8
Kushal K M <156884338+Kushal-Nasiwak@users.noreply.github.com>
2025-04-22 21:59:54 +0530

    Add tracker app and core models

---
+++ backend/tracker/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    phone_number = models.CharField(max_length=15 , blank=True , null=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    goal_per_day = models.IntegerField(default=1)
+    days_of_week = models.CharField(max_length=100 , help_text = "Comma-separated days: Mon,Tue,Wed...")
+    
+    def __str__(self):
+        return f"{self.name} - {self.user.username}"
+    
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    habit = models.ForeignKey(Habit , on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+    data =  models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class FocusSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField
+    
+    @property
+    def duration_minutes(self):
+        return (self.end_time - self.start_time).seconds // 60
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.duration_minutes} min"
+    
+class DailyLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    note = models.TextField(blank=True, null=True)
+    total_focus_minutes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
+    
