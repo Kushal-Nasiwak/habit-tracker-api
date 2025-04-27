9bd2cf2bde5939b55b54bb5155f68ec0a7c007e6
Kushal K M <156884338+Kushal-Nasiwak@users.noreply.github.com>
2025-04-27 14:48:43 +0530

    Add CORS support

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django.contrib.sites',
     'tracker',
-    
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -160,3 +162,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # (Optional, good for production)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CORS_ALLOW_ALL_ORIGINS = True
